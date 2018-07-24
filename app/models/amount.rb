f033456fe3fa6b39ce9e6b5ddf484464ca0db48c
Oladipupo Adeniran <oladipupo.adeniran@andela.com>
2018-07-24 02:06:06 +0100

    add specs config and model specs

--- app/models/amount.rb
+++ app/models/amount.rb
@@ -1,3 +1,5 @@
 class Amount < ApplicationRecord
   belongs_to :group
+
+  validates_presence_of :group
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :user_groups
-  belongs_to :groups, through: :user_groups
+  has_many :groups, through: :user_groups
 
   validates_presence_of :first_name, :last_name, :email, :password_digest
 end
